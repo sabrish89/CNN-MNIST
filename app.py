291345957b15b8801b22a6892b99ada5458a8172
Sabrish <sabrish89@gmail.com>
2018-07-12 03:37:02 +0800

    third commit

--- app.py
+++ app.py
@@ -85,8 +85,9 @@ def predict():
 
 if __name__ == "__main__":
 	#decide what port to run the app in
-	port = int(os.environ.get('PORT', 5000))
+	##port = int(os.environ.get('PORT', 5000))
 	#run the app locally on the givn port
-	app.run(host='0.0.0.0', port=port)
+	##app.run(host='0.0.0.0', port=port)
 	#optional if we want to run in debugging mode
 	#app.run(debug=True)
+	app.run(debug=True, use_reloader=True)
